ed3546f1f0e47f9a15763f0876d60f40855b608e
Brian Klein <bklein1981@gmail.com>
2023-10-16 14:44:35 -0500

    Configured webpack.config file with WebpackPwaManifest, HtmlWebpackPlugin, InjectManifest plugins, and module files

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,47 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new WebpackPwaManifest({
+        name: 'J.A.T.E.',
+        short_name: 'JATE',
+        start_url: './',
+        publicPath: './',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: path.resolve('./images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
+      }),
     ],
 
+    //CSS loaders and babel modules
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
       ],
     },
   };
