4e16963cb076fa609e3cc44bb85e09db14300b49
Brian Klein <bklein1981@gmail.com>
2023-10-18 13:52:59 -0500

    updated git ignore file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,30 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT from the database');
+
+  //Create a connection to the database
+  const jateDb = await openDB('jate', 1);
+
+  //create a new transaction
+  const tx = jateDb.transaction('jate', 'readwrite');
+
+  //retreive object store 'jate' from the transaction
+  const store = tx.objectStore('jate');
+
+  try {
+    // Add the content to the database
+    await store.add(content);
+
+    // Commit the transaction
+    await tx.done;
+    console.log('Content added to the database:', content);
+  } catch (error) {
+    console.error('Error adding content to the database:', error);
+  }
+
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
@@ -36,12 +59,5 @@ export const getDb = async () => {
   console.log('result.value', result)
   return result
 }
-
-
-
-
-
-
-console.error('getDb not implemented');
 
 initdb();
